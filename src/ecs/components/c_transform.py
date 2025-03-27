94c4b42d3b57b1d10b8b48bc3487e0f1a109b98d
pukinidev <v.caro@uniandes.edu.co>
2025-03-27 00:18:29 -0500

    first commit

---
+++ src/ecs/components/c_transform.py
@@ -0,0 +1,7 @@
+import pygame
+
+
+class CTransform:
+
+    def __init__(self, pos: pygame.Vector2) -> None:
+        self.pos = pos
---
+++ src/ecs/components/c_velocity.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+class CVelocity:
+    def __init__(self, vel: pygame.Vector2) -> None:
+        self.vel = vel
+        
+    
+        
