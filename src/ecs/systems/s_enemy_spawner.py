4a534f537cfdf434ed3eea738be8d01e5e7d02c4
pukinidev <v.caro@uniandes.edu.co>
2025-03-28 02:53:30 -0500

    fixo!

--- src/ecs/systems/s_enemy_spawner.py
+++ src/ecs/systems/s_enemy_spawner.py
@@ -11,14 +11,10 @@ def system_enemy_spawner(world: esper.World, delta_time: float, enemies_config: 
     
     for entity, (c_e,) in components:
         c_e.time_accumulator += delta_time
-
-        to_spawn = [event for event in c_e.spawn_events if c_e.time_accumulator >= event["time"]]
-
-        for event in to_spawn:
-            # TODO: Check why the position or velocity is affecting the bounce
-            position = pygame.Vector2(event["position"]["x"], event["position"]["y"])
-            enemy_type = event["enemy_type"]
-            crear_enemy(world, enemy_type, position, enemies_config)
-            print(f"[SPAWN] {enemy_type} at t={c_e.time_accumulator:.2f}s pos={position}")
-
-        c_e.spawn_events = [event for event in c_e.spawn_events if event not in to_spawn]
+        for i, event in enumerate(c_e.spawn_events):
+            if not c_e.spawned_flags[i] and c_e.time_accumulator >= event["time"]:
+                position = pygame.Vector2(event["position"]["x"], event["position"]["y"])
+                enemy_type = event["enemy_type"]
+                crear_enemy(world, enemy_type, position, enemies_config)
+                c_e.spawned_flags[i] = True  
+                print(f"[SPAWN] {enemy_type} at t={c_e.time_accumulator:.2f}s pos={position}")
