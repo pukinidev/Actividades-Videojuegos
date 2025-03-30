470e83ee94f9ef499d89a75eb91189e782f0e11d
pukinidev <v.caro@uniandes.edu.co>
2025-03-30 01:33:34 -0500

    fix base

--- src/ecs/systems/s_screen_bounce.py
+++ src/ecs/systems/s_screen_bounce.py
@@ -16,12 +16,12 @@ def system_screen_bounce(world: esper.World, screen: pygame.Surface):
         
         cuad_rect = c_s.surf.get_rect(topleft=c_t.pos)
         
-        if cuad_rect.left <= 0 or cuad_rect.right >= screen_rect.width:
+        if cuad_rect.left < 0 or cuad_rect.right > screen_rect.width:
             c_v.vel.x *= -1
             cuad_rect.clamp_ip(screen_rect)
             c_t.pos.x = cuad_rect.x
             
-        if cuad_rect.top <= 0 or cuad_rect.bottom >= screen_rect.height:
+        if cuad_rect.top < 0 or cuad_rect.bottom > screen_rect.height:
             c_v.vel.y *= -1
             cuad_rect.clamp_ip(screen_rect)
             c_t.pos.y = cuad_rect.y
